1e1382467f080771a837de1ccf77b7abc9957ce1
huyviet2582000 <102190252@sv1.dut.udn.vn>
2023-05-06 18:16:33 +0700

    feat: Migrate all hotel and reviews

--- api_user/services/profile.py
+++ api_user/services/profile.py
@@ -1,17 +1,83 @@
+import os
+import random
+import string
 from typing import Optional, List
 
-from django.contrib.auth.hashers import check_password
+from django.contrib.auth.hashers import check_password, make_password
 from django.db import transaction
 
-from api_user.models import Profile
+from api_user.models import Profile, Role
 from api_user.services import RoleService
 from api_user.services.token import TokenService
+from api_user.statics import RoleData
 
 
 class ProfileService:
     RANDOM_AVATARS = [
         "https://media-cdn.tripadvisor.com/media/photo-o/1a/f6/f0/48/default-avatar-2020-15.jpg",
         ""
+    ]
+    DUMP_FIRST_NAMES = {
+        Profile.GenderChoices.MALE: [
+            "Huy",
+            "Khang",
+            "Bảo",
+            "Minh",
+            "Phúc",
+            "Anh",
+            "Khoa",
+            "Phát",
+            "Đạt",
+            "Khôi",
+            "Long",
+            "Nam",
+            "Duy",
+            "Quân",
+            "Kiệt",
+            "Thịnh",
+            "Tuấn",
+            "Hưng",
+            "Hoàng",
+            "Hiếu",
+        ],
+        Profile.GenderChoices.FEMALE: [
+            "Anh",
+            "Trang",
+            "Linh",
+            "Phương",
+            "Hương",
+            "Thảo",
+            "Hà",
+            "Huyền",
+            "Ngọc",
+            "Hằng",
+            "Giang",
+            "Nhung",
+            "Yến",
+            "Nga",
+            "Mai",
+            "Thu",
+            "Hạnh",
+            "Vân",
+            "Hoa",
+            "Hiền",
+        ]
+    }
+    DUMP_LAST_NAMES = [
+        "Nguyễn",
+        "Trần",
+        "Lê",
+        "Phạm",
+        "Hoàng/Huỳnh",
+        "Phan",
+        "Vũ/Võ",
+        "Đặng",
+        "Bùi",
+        "Đỗ",
+        "Hồ",
+        "Ngô",
+        "Dương",
+        "Lý"
     ]
     @classmethod
     @transaction.atomic
@@ -78,7 +144,7 @@ class ProfileService:
         pass
 
     @classmethod
-    def create_dump_user_with_name(cls, reviewer_name: str):
+    def create_dump_user_with_name(cls, reviewer_name: str, role: RoleData, address: str = ""):
         names = reviewer_name.split(" ", 1)
         if len(names) == 2:
             first_name, last_name = names
@@ -86,3 +152,28 @@ class ProfileService:
             first_name = names
             last_name = ""
 
+        random_str = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(4))
+        avatar = random.choice(cls.RANDOM_AVATARS)
+        role = Role.objects.filter(name=role.value.get("name")).first()
+        email = f"bookingtravel_{first_name}_{random_str}@gmail.com"
+        user = Profile(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            avatar=avatar,
+            password=make_password(os.getenv('DEFAULT_PASSWORD')),
+            role=role,
+            gender=random.choice(Profile.GenderChoices.choices)[0],
+            address=address
+        )
+        user.save()
+
+        return user
+
+    @classmethod
+    def random_name(cls) -> str:
+        gender = random.choice([Profile.GenderChoices.MALE, Profile.GenderChoices.FEMALE])
+        last_name = random.choice(cls.DUMP_LAST_NAMES)
+        first_name = random.choice(cls.DUMP_FIRST_NAMES.get(gender))
+
+        return f"{first_name} {last_name}"
