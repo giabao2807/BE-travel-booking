7d85f24483556de9776c4fffca3276354db3535a
huyviet2582000 <102190252@sv1.dut.udn.vn>
2023-05-07 22:32:55 +0700

    feat: create merge hotel migration file

--- api_hotel/migrations/0004_alter_hotelreview_hotel_alter_hotelreview_owner.py
+++ api_hotel/migrations/0004_alter_hotelreview_hotel_alter_hotelreview_owner.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('api_hotel', '0003_alter_roomtype_beds_and_more'),
+        ('api_hotel', '0003_alter_roomtype_beds_alter_roomtype_square_and_more'),
     ]
 
     operations = [
